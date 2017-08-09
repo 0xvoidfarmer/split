c6af73474f7ee11dc8cd2fcce049fcfde9e7f4b1
0xvoidfarmer <30801219+0xvoidfarmer@users.noreply.github.com>
2017-08-09 16:05:24 +0100

    Update Split.sol

--- contracts/Split.sol
+++ contracts/Split.sol
@@ -1,30 +1,40 @@
 pragma solidity ^0.4.5;
-pragma solidity ^0.4.5;
 
 contract Split {
 	address public bob;
 	address public carol;
 
-	function Split(address _bob, address _carol){
-		bob = _bob;
-		carol = _carol;
+function Split(address _bob, address _carol){
+	bob = _bob;
+	carol = _carol;
+}
+
+
+function splitMoney () 
+	public 	
+	payable 
+	returns (bool success)
+	{
+		if(msg.value == 0 ) throw;
+		if(msg.sender == bob || msg.sender == carol) throw;
+		//Require bob address not empty
+		//require carol address not empty
+
+		//TODO: if (msg.value % 2 != 0) Do odd value split
+		var spiltAmount = (msg.value / 2);
+		if(!bob.send(spiltAmount)) throw; 
+		if(!carol.send(spiltAmount)) throw;
+
+		return true;
 	}
 
+function showBalance(address addr) 
+	returns (uint)
+	{
+	    return addr.balance;
+	}
 
-function splitMoney () public 	payable returns (bool success)
-{
-	if(msg.value == 0 ) throw;
-	if(msg.sender == bob || msg.sender == carol) throw;
-	
-	var spiltAmount = (msg.value / 2);
-	bob.send(spiltAmount);
-	carol.send(spiltAmount);
+function () {}
 
-	return true;
-}
-
-function showBalance(address addr) returns (uint){
-    return addr.balance;
-}
-
+//TODO: kill switch
 }
